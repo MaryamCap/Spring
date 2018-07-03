8602a452ec4ff64dda6a0d23a7d0ee8d9d6d2e5e
MaryamCap <moheb5159@gmail.com>
2018-07-03 11:46:04 -0500

    this is first spring framework project

---
+++ src/com/infotech/model/Message.java
@@ -0,0 +1,25 @@
+package com.infotech.model;
+
+public class Message {
+	
+	private int messageId;
+	private String message;
+	
+	
+	public int getMessageId() {
+		return messageId;
+	}
+	
+	public void setMessageId(int messageId) {
+		this.messageId = messageId;
+	}
+	
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+	public String getMessage() {
+		return message;
+	}
+	
+}
