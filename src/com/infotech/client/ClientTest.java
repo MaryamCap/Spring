230418c8a080c4ff7aaa6d640eca55f4a727a515
MaryamCap <moheb5159@gmail.com>
2018-07-05 11:04:02 -0500

    changed repository name

--- src/com/infotech/client/ClientTest.java
+++ src/com/infotech/client/ClientTest.java
@@ -19,7 +19,32 @@ public class ClientTest {
 			Message message = (Message)object;
 			System.out.println(message.getMessageId() + "\t" + message.getMessage());
 		}
+		System.out.println("---------------------------------------");
 		
+		Message message = beanFactory.getBean("message", Message.class);
+		System.out.println(message.getMessageId() + "\t" + message.getMessage());
+		
+		System.out.println("---------------------------------------");
+		
+		
+		
+		String[] aliases = beanFactory.getAliases("message");
+		for(String aName : aliases) {
+			System.out.println(aName);
+		}
+		
+		System.out.println("---------------------------------------");
+		
+		Message message1 = beanFactory.getBean(Message.class);
+		System.out.println(message1.getMessageId() + "\t" + message1.getMessage());
+		
+		System.out.println("---------------------------------------");
+		
+		Class<?> cls = beanFactory.getType("message");
+		System.out.println(cls.getName());
+		System.out.println("---------------------------------------");
+		
+		System.out.println(beanFactory.isSingleton("message"));
 
 	}
 
